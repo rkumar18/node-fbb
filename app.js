e28052e7eb9ce7f37a052c88e1ea6fe731432996
Rohit <rkrrko3@gmail.com>
2021-01-22 15:25:36 +0530

    res

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ const config = require("config");
 connect();
 app.use("/api", route);
 
+app.get("/", (req, res, next) => res.end("Hello this is heroku app"));
+
 app.listen(config.get("port") || process.env.PORT, () =>
   console.log(`fb-node listening on port ${config.get("port")}`)
 );
