b0371ef9ebabd2d5309b2c703ec3c746f9956262
Rohit <rkrrko3@gmail.com>
2021-01-22 15:21:52 +0530

    env port change

--- app.js
+++ app.js
@@ -6,6 +6,6 @@ const config = require("config");
 connect();
 app.use("/api", route);
 
-app.listen(config.get("port"), () =>
+app.listen(config.get("port") || process.env.PORT, () =>
   console.log(`fb-node listening on port ${config.get("port")}`)
 );
